a5585d0cd7189a197bc5909267a094ce33756bd1
lucaversa <lucajanini@hotmail.com>
2024-10-11 00:16:09 -0300

    tentando de novo...

--- render_build.sh
+++ render_build.sh
@@ -1,16 +1,19 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
+#!/bin/bash
+set -e  # Exit immediately if a command exits with a non-zero status
 
-echo "Starting build process"
+echo "Starting custom build process"
 
+# Install system dependencies
+apt-get update && apt-get install -y wget
+
+# Set up Chrome
 STORAGE_DIR=/opt/render/project/.render
 
 if [[ ! -d $STORAGE_DIR/chrome ]]; then
   echo "Downloading Chrome"
   mkdir -p $STORAGE_DIR/chrome
   cd $STORAGE_DIR/chrome
-  wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
   dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
   rm ./google-chrome-stable_current_amd64.deb
   echo "Chrome downloaded and extracted"
@@ -18,9 +21,10 @@ else
   echo "Using Chrome from cache"
 fi
 
-echo "Returning to project directory"
-cd $HOME/project/src
+# Return to project directory
+cd $RENDER_PROJECT_DIR
 
+# Install Python dependencies
 echo "Installing Python dependencies"
 pip install -r requirements.txt
 
